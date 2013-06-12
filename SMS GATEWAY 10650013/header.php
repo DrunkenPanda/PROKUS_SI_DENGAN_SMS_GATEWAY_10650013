94fd6079de8db1dff745434367a29f28c35f525e
A Dipo Prakoso <ADipoPrakoso@gmail.com>
2013-06-12 10:11:25 +0700

    sistem informasi absensi berbasis sms gateway
    si yang digunakan untuk absensi dan melaporkan kehadiran dengan mengirim
    sms dan menerimanya secara otomatis dengan php

---
+++ SMS GATEWAY 10650013/header.php
@@ -0,0 +1,28 @@
+<head>
+<title>Sistem Informasi SDN Boedjangan</title>
+<style type="text/css">
+<!--
+body {
+	background-image: url(gambar/backgroundweb.jpg);
+}
+body,td,th {
+	font-family: Times New Roman, Times, serif;
+	font-size: 18px;
+	color: #999999;
+}
+-->
+</style></head>
+<p><b> <img src="gambar/jeneng01.png"> </b></p>
+<p>Sistem Laporan Absensi Via SMS</p>
+
+<hr>
+<p>
+<ul>
+<li><a href="step1.php"><b>Database</b></a></li>
+<li><a href="step2.php"><b>Pengaturan Modem/Jaringan</b></a><br><br></li>
+<li><b><a href="kirim.php">Kirim SMS</a></b></li>
+<li><b><a href="terima.php">SMS YANG DITERIMA</a></b></li>
+</ul>
+</p>
+<hr>
+
